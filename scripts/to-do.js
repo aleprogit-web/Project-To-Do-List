fd1b8ab55de3dac1163851d76e2ee615219e9565
Alexandra <silvaalexandra123987@gmail.com>
2026-08-27 09:07:28 -0300

    add todo to list and delete all button

--- scripts/to-do.js
+++ scripts/to-do.js
@@ -1,6 +1,47 @@
 const addButton = document.querySelector('.js-add-button');
 
-addButton.addEventListener('click', () => {
-  const todo = document.querySelector('.js-to-do-input').innerText;
+let todoHistory = JSON.parse(localStorage.getItem('todoList')) || [];
 
+const todoList = document.querySelector('.to-do-list');
+
+const deleteAllButton = document.querySelector('.delete-all');
+
+
+function renderTodoList() {
+
+  let todoArray = '';
+
+  todoHistory.forEach((todo) => {
+    let html = 
+      `<div class="to-do-actions">
+        <button class="check-button"></button>
+        <p class="to-do">${todo}</p>
+        <img class="delete-button" src="images/delete-gray.png">
+      </div>`
+
+    todoArray += html;
+  })
+  todoList.innerHTML = todoArray;
+
+}
+
+addButton.addEventListener('click', () => {
+  const todo = document.querySelector('.js-to-do-input').value;
+  todoHistory.push(todo);
+  localStorage.setItem('todoList', JSON.stringify(todoHistory));
+  renderTodoList();
+
+  console.log(todoHistory);
 })
+
+deleteAllButton.addEventListener('click', () => {
+  todoList.innerHTML = '';
+  todoHistory = [];
+  localStorage.removeItem('todoList');
+
+  console.log(todoHistory);
+})
+
+renderTodoList();
+
+
