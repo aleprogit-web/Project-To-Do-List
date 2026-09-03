b3c330cd96103937fc8ab083faeb0516fe7a74b7
Alexandra <silvaalexandra123987@gmail.com>
2026-09-03 17:59:08 -0300

    add check button a function

--- scripts/to-do.js
+++ scripts/to-do.js
@@ -6,16 +6,35 @@ const todoList = document.querySelector('.to-do-list');
 
 const deleteAllButton = document.querySelector('.delete-all');
 
+const pendingFilter = document.querySelector('.js-pending-filter');
 
-function renderTodoList() {
+pendingFilter.addEventListener('click', () => {
+  const pendingTodos = todoHistory.filter((todo) => !todo.concluida);
+
+console.log(pendingTodos);
+
+ 
+})
+
+
+
+function renderTodoList(tasks = todoHistory) {
 
   let todoArray = '';
 
-  todoHistory.forEach((todo) => {
+  tasks.forEach((todo) => {
+    let taskClass;
+
+    if (todo.concluida) {
+      taskClass = 'completed';
+    } else {
+      taskClass = '';
+    }
+
     let html = 
-      `<div class="to-do-actions" 
+      `<div class="to-do-actions ${taskClass}" 
         data-id="${todo.id}">
-        <button class="check-button"></button>
+        <button class="check-button js-check-button"></button>
         <p class="to-do">${todo.texto}</p>
         <img class="delete-button js-delete-button" src="images/delete-gray.png">
       </div>`
@@ -23,6 +42,7 @@ function renderTodoList() {
     todoArray += html;
   })
   todoList.innerHTML = todoArray;
+
   const deleteEach = document.querySelectorAll('.js-delete-button');
 
   deleteEach.forEach((button) => {
@@ -36,11 +56,30 @@ function renderTodoList() {
     localStorage.setItem('todoList', JSON.stringify(todoHistory));
     renderTodoList();
 
+    })
+
   })
 
-})
+  const checkButton = document.querySelectorAll('.js-check-button');
+
+
+  checkButton.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const closestDiv = event.target.closest('.to-do-actions');
+      const matchingId = closestDiv.dataset.id;
+      const indice = todoHistory.findIndex((todo) => todo.id === matchingId);
+
+      todoHistory[indice].concluida =  !todoHistory[indice].concluida;
+      localStorage.setItem('todoList', JSON.stringify(todoHistory));
+
+      renderTodoList();
+
+      })
+
+  })
 
 }
+
 
 addButton.addEventListener('click', () => {
 
@@ -48,7 +87,8 @@ addButton.addEventListener('click', () => {
   const todo = document.querySelector('.js-to-do-input').value;
   todoHistory.push({
     texto: todo,
-    id: idNum
+    id: idNum,
+    concluida: false
   });
   localStorage.setItem('todoList', JSON.stringify(todoHistory));
   renderTodoList();
